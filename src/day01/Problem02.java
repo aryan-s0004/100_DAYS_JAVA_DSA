8e7632527e496c392260cdb91399d26031ec24aa
Aryan Sahu <aryansahu0004@gmail.com>
2025-12-10 14:40:04 +0530

    Day 02 - Initial DSA Problem

---
+++ src/day01/Problem02.java
@@ -0,0 +1,13 @@
+
+package day01;
+public class Problem02 {
+    public static int sum(int a, int b) {
+        return a + b;
+
+    }
+    public static void main(String[] args) {
+        System.out.println("WAP to return sum ");
+        int ans = sum(112, 27);
+        System.out.println("Sum = "+ans);
+    }
+}
