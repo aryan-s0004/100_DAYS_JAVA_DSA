699c8d8e1eb00cffbfe98af4023a1dc10287df8f
Aryan Sahu <aryansahu0004@gmail.com>
2025-12-20 16:30:58 +0530

    Day08 Topic -->  Find max. element in 2D Array & return index like (row,col)

---
+++ src/Arrays/Problem_22.java
@@ -0,0 +1,37 @@
+package Arrays;
+
+public class Problem_22 {
+
+    // Function to find maximum element and its index
+    public static void max2D(int[][] arr) {
+
+        int max = arr[0][0];
+        int row = 0;
+        int col = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+
+                if (arr[i][j] > max) {
+                    max = arr[i][j];
+                    row = i;
+                    col = j;
+                }
+            }
+        }
+
+        System.out.println("Maximum Element: " + max);
+        System.out.println("Index: (" + row + "," + col + ")");
+    }
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+                {1, 2, 3, 4},
+                {578, 6, 7, 8},
+                {9, 10, 11, 12}
+        };
+
+        max2D(matrix);
+    }
+}
