c3cfdae66c040fda0d36037f2851b6feaa371d9e
Aryan Sahu <aryansahu0004@gmail.com>
2026-01-07 00:29:45 +0530

    Implement fixed sliding window for subarray averages

---
+++ src/Sliding_Window/Problem_03.java
@@ -0,0 +1,37 @@
+package Sliding_Window;
+
+public class Problem_03 {
+
+    public static void avg_Subarr(int[] arr, int k) {
+
+        int n = arr.length;
+
+        if (n < k) {
+            System.out.println("Window cannot be formed");
+            return;
+        }
+
+        int windowSum = 0;
+        for (int i = 0; i < k; i++) {
+            windowSum += arr[i];
+        }
+
+        System.out.println((double) windowSum / k);
+
+        int left = 0;
+
+        for (int right = k; right < n; right++) {
+            windowSum = windowSum - arr[left] + arr[right];
+            left++;
+            System.out.println((double) windowSum / k);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {-5, -5, -5, -5, -5, -5, -5, -5};
+        int k = 3;
+
+        avg_Subarr(arr, k);
+    }
+}
