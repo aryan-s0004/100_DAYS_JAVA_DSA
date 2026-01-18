3c1581009832174c88035627c706717e4053f288
Aryan Sahu <aryansahu0004@gmail.com>
2026-01-19 00:11:04 +0530

    Applied basic cncept of OOPS like creating class, instance var. object crreation

--- src/OOPS/basic_oops.java
+++ src/OOPS/basic_oops.java
@@ -1,130 +1,70 @@
-//package OOPS;
-// Car class represents a real-world object
-//class Car {
-//    // Instance variables
-//    String name;
-//    String color;
-//
-//    // Default constructor
-//    // Called automatically when object is created
-//    Car() {
-//        System.out.println("Default constructor called");
-//    }
-//
-//    // Parameterized constructor
-//    // Used to initialize values at object creation
-//    Car(String name, String color) {
-//        this.name = name;   // current object's name
-//        this.color = color; // current object's color
-//    }
-//
-//    // Method to calculate average speed
-//    float avg(float x, float y, float z) {
-//        float average = (x + y + z) / 3;
-//        System.out.println("Car average speed = " + average);
-//        return average;
-//    }
-//
-//    class Car{
-//
-//        // Instance variables
-//        String name;
-//        String color;
-//
-//        // Default constructor
-//        // Called automatically when object is created
-//        Car() {
-//            System.out.println("Default constructor called");
-//        }
-//
-//        // Parameterized constructor
-//        // Used to initialize values at object creation
-//        Car(String name, String color) {
-//            this.name = name;   // current object's name
-//            this.color = color; // current object's color
-//        }
-//
-//        // Method to calculate average speed
-//        float avg(float x, float y, float z) {
-//            float average = (x + y + z) / 3;
-//            System.out.println("Car average speed = " + average);
-//            return average;
-//        }
-//
-//        // Copy constructor
-//        // Copies data from another Car object
-//        Car(Car c) {
-//            this.name = c.name;
-//            this.color = c.color;
-//        }
-//    }
-//}
-// Main class
-//public class basic_oops {
-//
-//    public static void main(String[] args) {
-//
-//        System.out.println("Basics of OOPS in Java\n");
-//
-//        // Object creation using default constructor
-//        Car c1 = new Car();
-//
-//        // Setting values using object reference
-//        c1.name = "Car1";
-//        c1.color = "Red";
-//
-//        System.out.println(c1.name);
-//        System.out.println(c1.color);
-//
-//        // Object creation using parameterized constructor
-//        Car c2 = new Car("Car2", "Green");
-//
-//        System.out.println(c2.name);
-//        System.out.println(c2.color);
-//
-//        // Object creation using copy constructor
-//        Car c3 = new Car(c2);
-//
-//        System.out.println(c3.name);
-//        System.out.println(c3.color);
-//
-//        // Calling method using objects
-//        float A = 100;
-//        float B = 200;
-//        float C = 300;
-//
-//        c1.avg(A, B, C);
-//        c2.avg(40.55F, 65.00F, 8.52F);
-//    }
-//    }
+package OOPS; // Package name to organize classes
 
+// User-defined class Car
+class Car {
 
-
-package OOPS;
-class Car{
-    int year;
-    String model;
-    String color;
-public static void main(String[] args) {
-    System.out.println("OOPS IN JAVA \n");
-    Car c1 =new Car();
-
-    c1.year=2019;
-    c1.model="Ford";
-    c1.color="red";
-
-    System.out.println(c1.year);
-    System.out.println(c1.model);
-    System.out.println(c1.color);
-
-    Car c2 =new Car();
-    c2.year=2018;
-    c2.model="Ford";
-    c2.color="red";
-
-    System.out.println(c2.year);
-    System.out.println(c2.model);
-    System.out.println(c2.color);
+    int year;       // Year of car
+    String model;   // Model name of car
+    String color;   // Color of car
 
 }
+
+// User-defined class Pen
+class Pen {
+
+    String color;   // Color of pen
+    String type;    // Type of pen
+
+    // Method to show pen action
+    void write() {
+        System.out.println("Hello world in java user define class ");
+    }
+
+}
+
+// Main class (program starts here)
+public class basic_oops {
+
+    public static void main(String[] args) {
+
+        System.out.println("OOPS IN JAVA\n"); // Print heading
+
+        // Creating first Car object
+        Car c1 = new Car();
+        c1.year = 2019;          // Set year
+        c1.model = "Ford";       // Set model
+        c1.color = "Red";        // Set color
+
+        // Printing c1 details
+        System.out.println(c1.year);
+        System.out.println(c1.model);
+        System.out.println(c1.color);
+
+        System.out.println("-------------"); // Separator
+
+        // Creating second Car object
+        Car c2 = new Car();
+        c2.year = 2018;          // Set year
+        c2.model = "Ford";       // Set model
+        c2.color = "Blue";       // Set color
+
+        // Printing c2 details
+        System.out.println(c2.year);
+        System.out.println(c2.model);
+        System.out.println(c2.color);
+
+        System.out.println("Class :- Pen "); // Pen class output
+
+        // Creating Pen object
+        Pen p1 = new Pen();
+        p1.color = "Black";      // Set pen color
+        p1.type = "Ballpoint";   // Set pen type
+
+        // Printing pen details
+        System.out.println(p1.color);
+        System.out.println(p1.type);
+
+        // Calling pen method
+        p1.write();
+    }
 }
